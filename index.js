7e19c79107a4c9e1e864aacbd17c158d69ba6e02
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-01-09 23:58:36 +0530

    stablizing

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/', (req , res)=>{
+    res.send("User Service")
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+ 
