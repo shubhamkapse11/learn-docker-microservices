59b8709617b87bb102de70f20c014bf29f532f4f
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-01-10 00:15:54 +0530

    verifying

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+// app.use(bodyParser.json());
 
 app.get('/', (req , res)=>{
     res.send("User Service")
